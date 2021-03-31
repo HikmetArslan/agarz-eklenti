05d1c3a2d11f2dc50e172f18828b3bc39fa18e6b
HikmetArslan <74685049+HikmetArslan@users.noreply.github.com>
2021-03-31 21:23:05 +0300

    Delete public/javascripts directory

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    /* Get the text field */
-    document.getElementById("code").select();
-    document.execCommand('copy');
-
-    /* Alert the copied text */
-    alert("Başarıyla kopyalandı! " + document.getElementById("cod2").getAttribute("key"));
-}
